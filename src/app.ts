6538d61e7ea321620c7a2147e458cbbb7699bdc5
Evgeniy B <shtk.pno@yandex.ru>
2025-07-23 11:38:55 +0300

    убрана задержка при вызове

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,6 @@ import type {
   ResourceIds,
   SkillsFullInfoIds,
   TypesOfWeaponIds,
-  ResourcesFullInfo,
   EnemiesList,
   ResourcesList,
   EnemiesIds,
@@ -35,11 +34,6 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-app.use(async (req, res, next) => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
   next();
 });
 
